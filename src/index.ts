ef67d5e4cd11f8be1fb81f2ca7dc3a9fe649ae51
praseth002 <prasethsokha@gmail.com>
2026-02-02 12:41:41 +0700

    fix: removed index

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,13 @@
-// import express from "express";
-
-// const app = express();
-// const PORT = 3000;
-
-// app.get("/", (req, res) => {
-//   res.send("🚀 Server is working!");
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`✅ Server running at http://localhost:${PORT}`);
-  
-// });
-
 import express from "express";
-import { connectDB } from "./config/pomotest";
-import pomodoroRoutes from "./routes/pomodoro.routes"; // adjust path if needed
 
 const app = express();
 const PORT = 3000;
 
-// middleware to parse JSON
-app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("🚀 Server is working!");
+});
 
-// routes
-app.use("/api/pomodoro", pomodoroRoutes);
-
-// connect DB
-connectDB();
-
-// start server
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
+  
 });
